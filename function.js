449c22994f3087719697e5ed5d6dd37e55515d4f
afiqTechno <afiqikmalahmadzainuddin@gmail.com>
2025-01-31 09:12:35 +0800

    Update function.js

--- function.js
+++ function.js
@@ -3,29 +3,29 @@ const confettiButton = document.getElementById('confettiButton');
 
 confettiButton.addEventListener('click', () => {
   // Use a confetti library (e.g., https://www.kirilv.com/canvas-confetti/)
-var end = Date.now() + (15 * 1000);
+ var end = Date.now() + (15 * 1000);
 
-// go Buckeyes!
-var colors = ['#bb0000', '#ffffff'];
-
-(function frame() {
-  confetti({
-    particleCount: 2,
-    angle: 60,
-    spread: 55,
-    origin: { x: 0 },
-    colors: colors
-  });
-  confetti({
-    particleCount: 2,
-    angle: 120,
-    spread: 55,
-    origin: { x: 1 },
-    colors: colors
-  });
-
-  if (Date.now() < end) {
-    requestAnimationFrame(frame);
-  }
-}());
+  // go Buckeyes!
+  var colors = ['#bb0000', '#ffffff'];
+  
+  (function frame() {
+    confetti({
+      particleCount: 2,
+      angle: 60,
+      spread: 55,
+      origin: { x: 0 },
+      colors: colors
+    });
+    confetti({
+      particleCount: 2,
+      angle: 120,
+      spread: 55,
+      origin: { x: 1 },
+      colors: colors
+    });
+  
+    if (Date.now() < end) {
+      requestAnimationFrame(frame);
+    }
+  }());
 });
